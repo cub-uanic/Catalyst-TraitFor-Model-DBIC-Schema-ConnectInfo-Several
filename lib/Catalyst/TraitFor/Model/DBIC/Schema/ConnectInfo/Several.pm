b91eef6f5aecf00a8ae4dc4e8e97b368849a8be4
Oleg Kostyuk <cub.uanic@gmail.com>
2009-12-04 12:52:54 +0200

    Initial creation with module-starter

---
+++ lib/Catalyst/TraitFor/Model/DBIC/Schema/ConnectInfo/Several.pm
@@ -0,0 +1,110 @@
+package Catalyst::TraitFor::Model::DBIC::Schema::ConnectInfo::Several;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Catalyst::TraitFor::Model::DBIC::Schema::ConnectInfo::Several - The great new Catalyst::TraitFor::Model::DBIC::Schema::ConnectInfo::Several!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Catalyst::TraitFor::Model::DBIC::Schema::ConnectInfo::Several;
+
+    my $foo = Catalyst::TraitFor::Model::DBIC::Schema::ConnectInfo::Several->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Oleg Kostyuk, C<< <cub at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-catalyst-traitfor-model-dbic-schema-connectinfo-several at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Catalyst-TraitFor-Model-DBIC-Schema-ConnectInfo-Several>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Catalyst::TraitFor::Model::DBIC::Schema::ConnectInfo::Several
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Catalyst-TraitFor-Model-DBIC-Schema-ConnectInfo-Several>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Catalyst-TraitFor-Model-DBIC-Schema-ConnectInfo-Several>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Catalyst-TraitFor-Model-DBIC-Schema-ConnectInfo-Several>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Catalyst-TraitFor-Model-DBIC-Schema-ConnectInfo-Several/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Oleg Kostyuk.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Catalyst::TraitFor::Model::DBIC::Schema::ConnectInfo::Several
---
+++ t/00-load.t
@@ -0,0 +1,9 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+    use_ok( 'Catalyst::TraitFor::Model::DBIC::Schema::ConnectInfo::Several' );
+}
+
+diag( "Testing Catalyst::TraitFor::Model::DBIC::Schema::ConnectInfo::Several $Catalyst::TraitFor::Model::DBIC::Schema::ConnectInfo::Several::VERSION, Perl $], $^X" );
